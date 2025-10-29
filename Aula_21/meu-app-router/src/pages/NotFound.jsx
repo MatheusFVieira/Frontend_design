5bb31e6014d4d2d72d8074b5bbc3317fb17506d4
MatheusFVieira <matheus.vf06@gmail.com>
2025-10-28 21:53:21 -0300

    Aula_21

---
+++ Aula_21/meu-app-router/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+function Home() {
+  return (
+    <div className="text-center p-8">
+      <p className="text-6xl text-gray-600">
+        404 - Página Não Encontrada
+      </p>
+      
+    </div>
+  );
+}
+
+export default Home;
